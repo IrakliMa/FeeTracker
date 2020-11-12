b914cb8720b464cf6e13f680a8efa58ddc75804f
Irakli <39024518+IrakliMa@users.noreply.github.com>
2020-11-12 19:37:05 +0000

    Final working

--- main.py
+++ main.py
@@ -1,48 +1,50 @@
 import requests
 import csv
 import time
+import datetime
 
-# Get Ethereum price in USD from CoinGecko
-request_price = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
-price = request_price.json()['ethereum']['usd']
+with open('fees.csv', 'w') as csvfile: # this will overwrite the already existing file
+  writer = csv.writer(csvfile)
 
-# Get gas data from EthGasStation
-request_data = requests.get('https://ethgasstation.info/api/ethgasAPI.json?api-key=10e6255a2d0d44851f2cea620e804b477fba1a13253909141b3f7b40024c')
-data = request_data.json()
+  # Write column titles
+  writer.writerow(["Time", "ETH Price", "Fast Gwei", "Average Gwei", "Slow Gwei", "Fast Mins", "Average Mins", "Slow Mins", "Fast tx USD", "Average tx USD", "Slow tx USD", "Fast erc tx USD", "Average erc tx USD", "Slow erc tx USD"])
 
-# with open('fees.csv', 'w') as csvfile:
-#     writer = csv.writer(csvfile)
-#     writer.writerow(["Ethereum:", price])
-#     writer.writerow(["Fast Gwei", "Fast Mins", "Fast tx USD", "Fast erc tx USD"])
+while(True):
+  # Get Ethereum price in USD from CoinGecko
+  request_price = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
+  price = request_price.json()['ethereum']['usd']
 
-# In Gwei
-fast_gwei = data['fast']/10
-average_gwei = data['average']/10
-slow_gwei = data['safeLow']/10
+  # Get gas data from EthGasStation
+  request_data = requests.get('https://ethgasstation.info/api/ethgasAPI.json?api-key=workswitheverything')
+  data = request_data.json()
 
-# Eth tx In USD
-#WTF? 21000000 != 1000000 * 21?
-fast_fee = round(fast_gwei * price / 1000000 * 21, 2) 
-average_fee = round(average_gwei * price / 1000000 * 21, 2)
-slow_fee = round(slow_gwei * price / 1000000 * 21, 2)
+  # In Gwei
+  fast_gwei = data['fast']/10
+  average_gwei = data['average']/10
+  slow_gwei = data['safeLow']/10
 
-# Erc20 tx In USD
-erc20_fast_fee = round(fast_gwei * price / 1000000 * 80, 2) 
-erc20_average_fee = round(average_gwei * price / 1000000 * 80, 2)
-erc20_slow_fee = round(slow_gwei * price / 1000000 * 80, 2)
+  # Eth tx In USD
+  #WTF? 21000000 != 1000000 * 21?
+  fast_fee = round(fast_gwei * price / 1000000 * 21, 2) 
+  average_fee = round(average_gwei * price / 1000000 * 21, 2)
+  slow_fee = round(slow_gwei * price / 1000000 * 21, 2)
 
+  # Erc20 tx In USD
+  erc20_fast_fee = round(fast_gwei * price / 1000000 * 80, 2) 
+  erc20_average_fee = round(average_gwei * price / 1000000 * 80, 2)
+  erc20_slow_fee = round(slow_gwei * price / 1000000 * 80, 2)
 
-# In Mins
-fast_time = data['fastWait']
-average_time = data['avgWait']
-slow_time = data['safeLowWait']
+  # In Mins
+  fast_time = data['fastWait']
+  average_time = data['avgWait']
+  slow_time = data['safeLowWait']
 
-print(fast_gwei, fast_time, fast_fee, erc20_fast_fee)
-with open('fees.csv', 'w') as csvfile:
+  date = datetime.datetime.today()
+
+  with open('fees.csv', 'a') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(["Ethereum:", price])
-    writer.writerow(["Fast Gwei", "Fast Mins", "Fast tx USD", "Fast erc tx USD"])
 
-    while(True):
-      writer.writerow([fast_gwei, fast_time, fast_fee, erc20_fast_fee])
-      time.sleep(6)
+    writer.writerow([date, price, fast_gwei, average_gwei, slow_gwei, fast_time, average_time, slow_time, fast_fee, average_fee, slow_fee, erc20_fast_fee,  erc20_average_fee, erc20_slow_fee])
+  
+  # Updates every 5 Mins
+  time.sleep(300)
